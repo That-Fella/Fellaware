376abd5fbd1cdcb8609b260742b2ca3d30925b53
That-Fella <163929324+That-Fella@users.noreply.github.com>
2024-06-28 17:05:13 -0500

    Create FellaHubLoader.lua

---
+++ Scripts/FellaHubLoader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6516141723 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/fella-hub/main/Scripts/FellaHubDoors.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/fella-hub/main/Scripts/FellaHubBot.lua"))()
+end
