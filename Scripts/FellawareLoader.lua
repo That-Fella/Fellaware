e035c0d471be041e78be10da5fce77782d1826e1
That-Fella <163929324+That-Fella@users.noreply.github.com>
2024-07-08 08:10:21 -0500

    Update FellawareLoader.lua

--- Scripts/FellawareLoader.lua
+++ Scripts/FellawareLoader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6839171747 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/fellaware/main/Scripts/FellawareDoorsV1.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/Fellaware/main/Scripts/FellawareDoorsV1.lua"))()
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/fellaware/main/Scripts/FellawareBot.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/That-Fella/Fellaware/main/Scripts/FellawareBot.lua"))()
 end
